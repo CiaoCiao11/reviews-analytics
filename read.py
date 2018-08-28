964c8f21d2a027cfcfe0b477b9520963e346cf84
CiaoCiao <kitty0607kitty@gmail.com>
2018-08-28 18:32:19 +0800

    average cal

--- read.py
+++ read.py
@@ -1,13 +1,14 @@
 data=[]
 count=0
+total=0
 with open ('reviews.txt','r') as f:
     for line in f:
         data.append(line)
+        total += len(line)
         count += 1
         if count% 1000 == 0:
         	print(len(data))
        
-print(len(data))
-print(data[0])
-print('-----------------------')
-print(data[1])
+print('檔案讀完了，共有',len(data),'筆資料')
+average = total / len(data)
+print('平均資料長度',average)
